1d29d60fa60ea462156a0f936b2c4026a83d5ae7
Isidro Vasquez <isidroevc@gmail.com>
2019-09-22 00:24:10 -0500

    aversinotruenaelgit

---
+++ base.sql
@@ -0,0 +1,8 @@
+create table contactos(
+    id int not null auto_increment primary key,
+    nombre varchar(100),
+    apellidos varchar(100),
+    email varchar(50),
+    telefono_fijo varchar(15),
+    telefono_celular varchar(15)
+);
---
+++ vistas/CrearContactoVista.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Crear Contacto</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Crear contacto</h1>
+        <form action="../controladores/CrearContactoControlador.php" method="POST">
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="emailHelp"
+                    placeholder="Escribe el nombre del contacto">
+                <small id="emailHelp" class="form-text text-muted">Escribe el nombre del contacto</small>
+            </div>
+            <div class="form-group">
+                <label for="apellidos">Apellidos</label>
+                <input type="text" class="form-control" id="apellidos" name="apellidos" placeholder="Apellidos">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="telefono">Telefono Fijo</label>
+                <input type="text" class="form-control" id="telefono_fijo" name="telefono_fijo" placeholder="Telefono Fijo">
+            </div>
+            <div class="form-group">
+                <label for="telefono">Telefono Celular</label>
+                <input type="text" class="form-control" id="telefono_celular" name="telefono_celular" placeholder="Celular">
+            </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </form>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
